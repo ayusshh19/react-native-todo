ecedecfb9ed857ea5a2430e5c3ec9fd01cdcaa91
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-03-04 18:57:18 +0530

    initial setup

---
+++ screens/Startpage.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Startpage() {
+  return (
+    <View>
+        <Text>
+            Starting page
+        </Text>
+    </View>
+  )
+}
